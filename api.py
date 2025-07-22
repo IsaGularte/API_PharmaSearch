46bee9f9822629d437cd0ff608ab628c0826f7ea
IsaGularte <isagularte09@gmail.com>
2025-07-22 02:34:59 -0300

    Adiciona API com Procfile e dependências

--- api.py
+++ api.py
@@ -34,7 +34,7 @@ def criar_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-   service = Service(ChromeDriverManager().install())
+    service = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=service, options=chrome_options)
 
 @app.route('/comparar_precos', methods=['GET'])
